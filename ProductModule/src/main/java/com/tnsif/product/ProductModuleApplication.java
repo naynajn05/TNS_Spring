c4237e25c49ec36acc2ac21a6a5038c204fb9daa
HP <HP@LAPTOP-N6BHAJN2>
2025-06-01 19:51:31 +0530

    Product Module-Sprint 1

---
+++ ProductModule/src/main/java/com/tnsif/product/ProductModuleApplication.java
@@ -0,0 +1,14 @@
+//Driver Class
+package com.tnsif.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductModuleApplication.class, args);
+	}
+
+}
